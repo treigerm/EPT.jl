9a502c55b21e12bee3d079cbb332669af1ccd3cd
Tim Reichelt <treichelt6@gmail.com>
2020-07-28 08:43:48 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.6

---
+++ src/ExpectationProgramming.jl
@@ -0,0 +1,5 @@
+module ExpectationProgramming
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ExpectationProgramming
+using Test
+
+@testset "ExpectationProgramming.jl" begin
+    # Write your tests here.
+end
