75a11a5a0f8f3edb88d1869bbf83ae6391d60408
Tim Reichelt <treichelt6@gmail.com>
2020-07-28 11:12:21 +0200

    Initial implementation of creating three Turing models

--- src/ExpectationProgramming.jl
+++ src/ExpectationProgramming.jl
@@ -1,5 +1,75 @@
 module ExpectationProgramming
 
-# Write your package code here.
+# NOTE: Things don't work if we do "import Turing" because of some scoping 
+# behaviour in the macros.
+using Turing
+
+import MacroTools
+
+export @expectation,
+    Expectation
+
+struct Expectation
+    gamma1_pos
+    gamma1_neg
+    gamma2
+end
+
+# TODO: Handle multiple return values, e.g. return a, b, c
+macro expectation(expr)
+    fn_dict = MacroTools.splitdef(expr)
+
+    fn_name = fn_dict[:name]
+    expct1_pos_name = gensym(fn_name)
+    expct1_neg_name = gensym(fn_name)
+    expct2_name = gensym(fn_name)
+
+    fn_dict[:name] = expct2_name
+    fn_expr_expct2 = MacroTools.combinedef(fn_dict)
+
+    fn_body = fn_dict[:body]
+    fn_dict[:body] = translate_return(fn_body, true)
+    fn_dict[:name] = expct1_pos_name
+    fn_expr_expct1_pos = MacroTools.combinedef(fn_dict)
+
+    fn_dict[:body] = translate_return(fn_body, false)
+    fn_dict[:name] = expct1_neg_name
+    fn_expr_expct1_neg = MacroTools.combinedef(fn_dict)
+
+    return quote
+        esc(Turing.@model($fn_expr_expct1_pos))
+
+        esc(Turing.@model($fn_expr_expct1_neg))
+
+        esc(Turing.@model($fn_expr_expct2))
+
+        $(esc(fn_name)) = Expectation(
+            $(expct1_pos_name),
+            $(expct1_neg_name),
+            $(expct2_name)
+        )
+    end
+end
+
+function translate_return(expr, is_positive_expectation)
+    MacroTools.postwalk(expr) do e
+        if MacroTools.@capture(e, return r_)
+            tmp_var = gensym()
+            # NOTE: The compiler is smart enough to remove the if condition.
+            # TODO: Possibly use the macro @addlogp!() to get rid of warning
+            quote
+                $(tmp_var) = $r
+                if $is_positive_expectation
+                    Turing.acclogp!(_varinfo, log(max($(tmp_var), 0)))
+                else
+                    Turing.acclogp!(_varinfo, log(-min($(tmp_var), 0)))
+                end
+                return $(tmp_var)
+            end
+        else
+            e
+        end
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,33 @@
 using ExpectationProgramming
+using Turing
 using Test
+using Random
+
+#rng = MersenneTwister(42)
+Random.seed!(42)
 
 @testset "ExpectationProgramming.jl" begin
-    # Write your tests here.
+    @expectation function expct()
+        x ~ Normal(0, 1) 
+        y ~ Normal(x, 1)
+        return x^2
+    end
+
+    xval = 2
+    yval = 1
+    vi = Turing.VarInfo(expct.gamma1_pos())
+    vi[@varname(x)] = [xval;]
+    vi[@varname(y)] = [yval;]
+
+    gamma2_lp = logpdf(Normal(0, 1), xval) + logpdf(Normal(xval, 1), yval) 
+    @test expct.gamma2()(vi) == xval^2
+    @test Turing.getlogp(vi) == gamma2_lp
+
+    gamma1_pos_lp = gamma2_lp + log(max(xval^2, 0))
+    @test expct.gamma1_pos()(vi) == xval^2
+    @test Turing.getlogp(vi) == gamma1_pos_lp
+
+    gamma1_neg_lp = gamma2_lp + log(-min(xval^2, 0))
+    @test expct.gamma1_neg()(vi) == xval^2 # TODO: Should it return 0 instead?
+    @test Turing.getlogp(vi) == gamma1_neg_lp
 end
