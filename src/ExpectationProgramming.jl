1f3bbd136e7288398d86dcc3cb339fae9e14475a
Tim Reichelt <treichelt6@gmail.com>
2020-07-30 10:55:14 +0200

    Integrate with AnnealedIS

--- src/ExpectationProgramming.jl
+++ src/ExpectationProgramming.jl
@@ -3,16 +3,32 @@ module ExpectationProgramming
 # NOTE: Things don't work if we do "import Turing" because of some scoping 
 # behaviour in the macros.
 using Turing
+using AnnealedIS
+using Random
 
 import MacroTools
 
 export @expectation,
-    Expectation
+    Expectation,
+    TABI,
+    AIS,
+    estimate
 
+# TODO: What are the correct types for the fields? They are Turing models.
+# They can either be a turing model or a turing modelgen.
 struct Expectation
     gamma1_pos
     gamma1_neg
     gamma2
+end
+
+function (expct::Expectation)(args...; kwargs...)
+    # TODO: Check that expct contains Turing.ModelGen's.
+    return Expectation(
+        expct.gamma1_pos(args...; kwargs...),
+        expct.gamma1_neg(args...; kwargs...),
+        expct.gamma2(args...; kwargs...)
+    )
 end
 
 # TODO: Handle multiple return values, e.g. return a, b, c
@@ -70,6 +86,58 @@ function translate_return(expr, is_positive_expectation)
             e
         end
     end
+end
+
+struct TABI{T<:Turing.InferenceAlgorithm}
+    estimation_alg::T
+end
+
+struct AIS <: Turing.InferenceAlgorithm
+    num_samples::Int
+    num_annealing_dists::Int
+end
+
+# TODO: Check the Distributions package and how estimate is used there. 
+# Potentially rename this function.
+function Distributions.estimate(expct::Expectation, alg::TABI{AIS})
+    ais = AnnealedISSampler(expct.gamma2, alg.estimation_alg.num_annealing_dists)
+    prior_density = ais.prior_density
+    samples = ais_sample(Random.GLOBAL_RNG, ais, alg.estimation_alg.num_samples)
+    Z2 = normalisation_constant(samples)
+
+    ais = AnnealedISSampler(
+        rng -> AnnealedIS.sample_from_prior(rng, expct.gamma1_pos), 
+        prior_density,
+        AnnealedIS.make_log_joint_density(expct.gamma1_pos),
+        alg.estimation_alg.num_annealing_dists
+    )
+    samples = ais_sample(Random.GLOBAL_RNG, ais, alg.estimation_alg.num_samples)
+    Z1_positive = normalisation_constant(samples)
+
+    ais = AnnealedISSampler(
+        rng -> AnnealedIS.sample_from_prior(rng, expct.gamma1_neg),
+        prior_density,
+        AnnealedIS.make_log_joint_density(expct.gamma1_neg),
+        alg.estimation_alg.num_annealing_dists
+    )
+    samples = ais_sample(Random.GLOBAL_RNG, ais, alg.estimation_alg.num_samples)
+    Z1_negative = normalisation_constant(samples)
+
+    return (Z1_positive - Z1_negative) / Z2
+end
+
+function estimate_normalisation_constant(model::Turing.Model, alg::AIS)
+    ais = AnnealedISSampler(model, alg.num_annealing_dists)
+    # TODO: Have nicer way to pass in rng.
+    samples = ais_sample(Random.GLOBAL_RNG, ais, alg.num_samples)
+    return normalisation_constant(samples)
+end
+
+function normalisation_constant(samples)
+    sum_weights = sum(samples) do weighted_sample
+        exp(weighted_sample.log_weight)
+    end
+    return sum_weights / length(samples)
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,27 +7,77 @@ using Random
 Random.seed!(42)
 
 @testset "ExpectationProgramming.jl" begin
-    @expectation function expct()
-        x ~ Normal(0, 1) 
-        y ~ Normal(x, 1)
-        return x^2
+    @testset "Expectation Macro" begin
+        @expectation function expct(y)
+            x ~ Normal(0, 1) 
+            y ~ Normal(x, 1)
+            return x^2
+        end
+
+        yval = 1
+        expct_conditioned = expct(yval)
+
+        xval = 2
+        vi = Turing.VarInfo(expct_conditioned.gamma1_pos)
+        vi[@varname(x)] = [xval;]
+        #vi[@varname(y)] = [yval;]
+
+        # Check that the three different models return the right score for a given trace.
+        gamma2_lp = logpdf(Normal(0, 1), xval) + logpdf(Normal(xval, 1), yval) 
+        @test expct_conditioned.gamma2(vi) == xval^2
+        @test Turing.getlogp(vi) == gamma2_lp
+
+        gamma1_pos_lp = gamma2_lp + log(max(xval^2, 0))
+        @test expct_conditioned.gamma1_pos(vi) == xval^2
+        @test Turing.getlogp(vi) == gamma1_pos_lp
+
+        gamma1_neg_lp = gamma2_lp + log(-min(xval^2, 0))
+        @test expct_conditioned.gamma1_neg(vi) == xval^2 # TODO: Should it return 0 instead?
+        @test Turing.getlogp(vi) == gamma1_neg_lp
     end
 
-    xval = 2
-    yval = 1
-    vi = Turing.VarInfo(expct.gamma1_pos())
-    vi[@varname(x)] = [xval;]
-    vi[@varname(y)] = [yval;]
+    @testset "Broken test" begin
+        """
+        This does not work because somehow I haven't got it to work that the 
+        code in the function body gets evaluated in the scope of the user module.
+        So here when calling expct() f is not in scope, even though it should 
+        work.
+        """
 
-    gamma2_lp = logpdf(Normal(0, 1), xval) + logpdf(Normal(xval, 1), yval) 
-    @test expct.gamma2()(vi) == xval^2
-    @test Turing.getlogp(vi) == gamma2_lp
+        #f(x) = x^2
+        #@expectation function expct()
+        #    x ~ Normal(0, 1) 
+        #    y ~ Normal(x, 1)
+        #    return f(x)
+        #end
+        
+        #xval = 2
+        #yval = 1
+        #vi = Turing.VarInfo(expct.gamma1_pos())
+        #vi[@varname(x)] = [xval;]
+        #vi[@varname(y)] = [yval;]
+    end
 
-    gamma1_pos_lp = gamma2_lp + log(max(xval^2, 0))
-    @test expct.gamma1_pos()(vi) == xval^2
-    @test Turing.getlogp(vi) == gamma1_pos_lp
+    @testset "Expectation Estimation" begin
+        @expectation function expct(y)
+            x ~ Normal(0, 1) 
+            y ~ Normal(x, 1)
+            return x^2
+        end
+        
+        yval = 3
+        expct_conditioned = expct(yval)
 
-    gamma1_neg_lp = gamma2_lp + log(-min(xval^2, 0))
-    @test expct.gamma1_neg()(vi) == xval^2 # TODO: Should it return 0 instead?
-    @test Turing.getlogp(vi) == gamma1_neg_lp
+        num_annealing_dists = 10
+        num_samples = 10
+
+        tabi = TABI(
+            AIS(num_annealing_dists, num_samples)
+        )
+
+        expct_estimate = estimate(expct_conditioned, tabi)
+        @test !isnan(expct_estimate)
+    end
+
+    # TODO: TABI convergence test.
 end
